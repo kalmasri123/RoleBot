7416a06fb06680c95a6b75833b6bc1ded03d51d0
kalmasri123 <kalmasri@me.com>
2019-07-26 09:55:26 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'json'});
+  console.log(req.body)
   res.write({auth:true});
   res.end();
 }).listen(process.env.PORT);
