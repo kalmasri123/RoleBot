e53c1f8f089e1516a15228ef8150ea53f354f059
kalmasri123 <kalmasri@me.com>
2019-07-26 10:02:06 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write('Hello World!');
   res.end();
-}).listen(process.env.PORT,8080);
+}).listen(process.env.PORT||8080);
 
 
 
